6556a87f957bd870333a0aae4188aede6464a404
Jorit Studer <jorit.studer@gmail.com>
2023-07-28 00:49:17 +0200

    more refactoring

--- src/model/gce_layer.py
+++ src/model/gce_layer.py
@@ -1,5 +1,4 @@
 import torch
-from torch import Tensor
 from torch_geometric.nn import GATConv, GCNConv
 
 
@@ -7,28 +6,32 @@ class BaseGCELayer(torch.nn.Module):
     def __init__(
         self,
         field_dims: int,
-        embed_dim: int,
-        features: Tensor,
-        A: Tensor,
+        embedding_dim: int,
+        features: torch.Tensor,
+        attention_vec: torch.Tensor,
         attention: bool = False,
+        attention_dropout: float = 0.4,
+        num_multi_head_attentions: int = 8,
         cooccurrence_weight: float = 1.0,
     ) -> None:
         super().__init__()
-        self.A = A
+        self.attention_vec = attention_vec
         self.features = features
         self.cooccurrence_weight = cooccurrence_weight
 
         if attention:
-            self.GCN_module = GATConv(
-                in_channels=int(field_dims),
-                out_channels=embed_dim,
-                heads=8,
-                dropout=0.4,
+            self.graph_conv = GATConv(
+                in_channels=field_dims,
+                out_channels=embedding_dim,
+                heads=num_multi_head_attentions,
+                dropout=attention_dropout,
             )
         else:
-            self.GCN_module = GCNConv(in_channels=field_dims, out_channels=embed_dim)
+            self.graph_conv = GCNConv(
+                in_channels=field_dims, out_channels=embedding_dim
+            )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError(
             "Forward method must be implemented in derived classes."
         )
@@ -37,25 +40,25 @@ class BaseGCELayer(torch.nn.Module):
 class GCELayerDishIngredient(BaseGCELayer):
     def __init__(
         self,
-        field_dims,
-        embedding_dim,
-        features_dish_ingredient,
-        A_dish_ingredient,
-        attention=False,
-        cooccurrence_weight=1.0,
+        field_dims: int,
+        embedding_dim: int,
+        features_dish_ingredient: torch.Tensor,
+        attention_vec_dish_ingredient: torch.Tensor,
+        attention: bool = False,
+        cooccurrence_weight: float = 1.0,
     ) -> None:
         super().__init__(
             field_dims,
             embedding_dim,
             features_dish_ingredient,
-            A_dish_ingredient,
+            attention_vec_dish_ingredient,
             attention=attention,
             cooccurrence_weight=cooccurrence_weight,
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Custom forward pass for dish-ingredient interactions
-        gce_embeddings = self.GCN_module(self.features, self.A)[x]
+        gce_embeddings = self.graph_conv(self.features, self.attention_vec)[x]
         user_ingredient_embeddings = torch.mul(gce_embeddings, self.cooccurrence_weight)
         return user_ingredient_embeddings
 
@@ -63,23 +66,23 @@ class GCELayerDishIngredient(BaseGCELayer):
 class GCELayerUserDish(BaseGCELayer):
     def __init__(
         self,
-        field_dims,
-        embedding_dim,
-        features_user_dish,
-        A_user_dish,
-        attention=False,
-        cooccurrence_weight=1.0,
+        field_dims: int,
+        embedding_dim: int,
+        features_user_dish: torch.Tensor,
+        attention_vec_user_dish: torch.Tensor,
+        attention: bool = False,
+        cooccurrence_weight: float = 1.0,
     ) -> None:
         super().__init__(
             field_dims,
             embedding_dim,
             features_user_dish,
-            A_user_dish,
+            attention_vec_user_dish,
             attention=attention,
             cooccurrence_weight=cooccurrence_weight,
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Custom forward pass for user-dish interactions
-        gce_embeddings = self.GCN_module(self.features, self.A)[x]
-        return gce_embeddings
+        gce_embeddings = self.graph_conv(self.features, self.attention_vec)[x]
+        return torch.Tensor(gce_embeddings)
